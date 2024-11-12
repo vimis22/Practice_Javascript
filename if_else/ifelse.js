994de5488d4688c6bbe0d67a488bf9fd8e910163
Vivek Misra <115539722+vimis22@users.noreply.github.com>
2024-11-12 21:27:27 +0100

    Working on If-Else Statement Exercise Tasks.
    Working on If-Else Statement Exercise Tasks.

--- if_else/ifelse.js
+++ if_else/ifelse.js
@@ -1,29 +1,111 @@
 //Opgave 1
-let input1 = prompt("Please write a number: ");
-println("You have inserted the number: " + input);
-input1 = Number(input1);
-
-function numberDefinition(input1){
-    if(input % 2 == 0){
-        console.log("The Number: " + input1 + " is Even");
-    }else{
-        console.log("The Number: " + input1 + " is Odd");
+function checkEvenOdd(number) {
+    if (number % 2 === 0) {
+        console.log("Even");
+    } else {
+        console.log("Odd");
     }
 }
-numberDefinition(input1);
 
 //Opgave 2
-let input2 = prompt("Please write a number: ");
-println("You have inserted the number: " + input2);
-input2 = Number(input2);
-
-function numberCategory(input2){
-    if(input2 => 0){
-        console.log("The Number " + input2 + " is positive");
-    }else if (input2 <= 0){
-        console.log("The Number " + input2 + " is negative");
+function checkNumber(num) {
+    if (num > 0) {
+        console.log("Positive");
+    } else if (num < 0) {
+        console.log("Negative");
+    } else {
+        console.log("Zero");
     }
 }
 
-numberCategory(input2);
 
+//Opgave 3
+function checkVowelConsonant(char){
+    if(["a","e","i","o","u"].includes(char.toLocaleLowerCase())){
+        console.log("Vowel");
+    }else{
+        console.log("Consonant");
+    }
+}
+
+//Opgave 4
+function classifyGrade(score){
+    if (score>=90){
+        console.log("A");
+    }else if (score>=90){
+        console.log("B");
+    }else if (score>=70){
+        console.log("C");
+    }else if (score>=60){
+        console.log("D");
+    }else{
+        console.log("F");
+    }
+}
+
+//Opgave 5
+function checkDivisibility(num){
+    if (num % 5 === 0 && num % 10 === 0){
+        console.log("Divisible by 5 and 10");
+    }else{
+        console.log("Not Divisible by 5 and 10");
+    }
+}
+
+//Opgave 6
+function checkTemperature(temp){
+    if (temp > 30){
+        console.log("Hot");
+    }else if (temp >= 15){
+        console.log("Warm");
+    }else {
+        console.log("Cold");
+    }
+}
+
+//Opgave 7
+function checkCase(char){
+    if (char === char.toUpperCase()){
+        console.log("Uppercase");
+    }else{
+        console.log("Lowercase");
+    }
+}
+
+//Opgave 8
+function checkLeapYear(year){
+    if ((year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0 )){
+        console.log("Leap Year");
+    }else{
+        console.log("Not a Leap Year");
+    }
+}
+
+//Opgave 9
+function checkTeenager(age){
+    if (age>=13 && age<=19){
+        console.log("Teenager");
+    }else{
+        console.log("Not a Teenager");
+    }
+}
+
+//Opgave 10
+function login(username, password){
+    if(username === "admin" && password === "password123"){
+        console.log("Access Granted");
+    }else{
+        console.log("Access Denied");
+    }
+}
+
+checkEvenOdd(4);
+checkNumber(-10);
+checkVowelConsonant("a");
+classifyGrade(85);
+checkDivisibility(20);
+checkTemperature(25);
+checkCase("A");
+checkLeapYear(2024);
+checkTeenager(16);
+login("admin","password123");
